da8928787b59b740a5c9c4651bd49d2dce07a856
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-01-19 21:03:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,13 +13,23 @@ https://github.com/ohmyzsh/ohmyzsh
 Php-env : https://github.com/phpenv/phpenv
 Node : https://docs.volta.sh
 
-########
-# Data #
-########
+#############
+# Databases #
+#############
 # Redis
 curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 apt-get update && apt-get install redis
+redis-server &
+
+# MongoDb
+curl -fsSL https://pgp.mongodb.com/server-4.4.asc | gpg -o /usr/share/keyrings/mongodb-server-4.4.gpg --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+# -> select the Geographic area 8 "Europe", 37 "Paris"
+apt update && apt install -y mongodb-org
+#export MONGO_URI=mongodb://root:root@localhost:27017
+mongod --dbpath /data/db --port 27017 --fork --logpath /var/log/mongodb/mongod.log
+mongo admin --eval 'db.createUser({user:"root",pwd:"root",roles:[{role:"userAdminAnyDatabase",db:"admin"}]})'
 
 #########
 # Tools #
