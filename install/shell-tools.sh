1f9043a013e944f579917d9ab14ae4fac1b290ef
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-05-17 21:52:12 +0200

    Update shell-tools.sh

--- install/shell-tools.sh
+++ install/shell-tools.sh
@@ -83,6 +83,10 @@ apt install -y jq
 
 ### Nixos @https://nixos.org
 
+### Kubectl ###
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+
 ####################
 # Network & Server #
 ####################
