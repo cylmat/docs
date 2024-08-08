3a79e703d8590081fc79fd7ff868f6a18af6c5a7
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-08-08 19:32:20 +0200

    Update shell-tools.sh

--- install/shell-tools.sh
+++ install/shell-tools.sh
@@ -4,7 +4,7 @@
 sudo apt-get install ninja-build gettext cmake unzip curl
 sudo apt install -y software-properties-common
 
-#########
+##########################################################################
 # Shell #
 #########
 
@@ -18,15 +18,22 @@ apt install -y fish
 
 ### Zsh @https://www.zsh.org
 # @https://github.com/unixorn/awesome-zsh-plugins
-apt install -y fonts-powerline zsh
-# OhMyZsh : @https://ohmyz.sh
+sudo apt install -y zsh
 
+# OhMyZsh : @https://ohmyz.sh
 # zsh-omz:
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # zsh-autosuggestions:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # zsh-syntax-highlighting:
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Powerline
+apt install -y fonts-powerline
+wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+mv PowerlineSymbols.otf /usr/share/fonts/
+fc-cache -vf
+mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 
 ### Starship @https://starship.rs
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
@@ -38,14 +45,20 @@ curl -s https://ohmyposh.dev/install.sh | bash -s
 oh-my-posh get shell
 echo 'eval "$(oh-my-posh init bash)"' >> ~/.bashrc
 
-### NVM + Node
-# Volta 
-curl https://get.volta.sh | bash
-volta install node
+# BASH-it #
+# https://github.com/Bash-it/bash-it
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it && ~/.bash_it/install.sh
 
-###########
-# Edition #
-###########
+### ble.sh
+# https://github.com/akinomyoga/ble.sh
+apt update && apt install -y gawk
+git clone --recursive https://github.com/akinomyoga/ble.sh.git
+make -C ble.sh install PREFIX=~/.local
+echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
+
+##########################################################################
+# Edition VIM #
+###############
 
 ### Vim @https://www.vim.org
 # @https://vimawesome.com
@@ -54,8 +67,18 @@ apt install -y vim
 # vundle: https://github.com/VundleVim/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# VIM Plug #
+
 # https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# --- Autosuggestion :
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# plugins=(zsh-autosuggestions)
+
+#--- syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# plugins=( ...zsh-syntax-highlighting)
 
 ### ###
 
@@ -69,7 +92,7 @@ apt install -y tmux
 
 ### @https://doc.ubuntu-fr.org/awesome
 
-#########
+##########################################################################
 # Tools #
 #########
 
@@ -95,67 +118,42 @@ apt-get install -y dos2unix
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
-####################
+##########################################################################
 # Network & Server #
 ####################
 
 # @https://nmap.org: Security scanner
 # @https://www.ntop.org: Network monitor
 # Netstat (ss) @use : ss -plunt
-apt install -y net-tools  nmap
+sudo apt install -y net-tools nmap
 
 ### Ajenti @http://docs.ajenti.org/en/latest/man/install.html
 
 ### Lynx @https://lynx.invisible-island.net/
-# sudo apt-get install lynx
+sudo apt install -y lynx
 
-### W3m
-# sudo apt install w3m w3m-img
+### W3m terminal browser
+sudo apt install -y w3m w3m-img
 
+### NVM + Node
+# Volta 
+curl https://get.volta.sh | bash
+volta install node
 
-
-# # # cleanup # # #
-
-
-
-########
-# BASH #
-########
-# https://github.com/Bash-it/bash-it
-bash-it:
-	git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it && ~/.bash_it/install.sh
- 
-# https://github.com/akinomyoga/ble.sh
-blesh:
-	apt update && apt install -y gawk
-	git clone --recursive https://github.com/akinomyoga/ble.sh.git
-	make -C ble.sh install PREFIX=~/.local
-	echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
-
-#######
+##########################################################################
 # DEV #
 #######
-composer-bin:
-	php -r 'copy("https://composer.github.io/installer.sig", "/tmp/installer.sig");'
-	php -r 'copy("https://getcomposer.org/installer", "composer-setup.php");'
-	php -r "if (hash_file('sha384', 'composer-setup.php') === file_get_contents('/tmp/installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('/tmp/composer-setup.php'); } echo PHP_EOL;"
-	php composer-setup.php --install-dir=bin --filename=composer
-	php -r "unlink('composer-setup.php'); unlink('/tmp/installer.sig');"
-	
-#########
-# FONTS #
-#########
-# Powerline
-apt install -y fonts-powerline
-wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
-mv PowerlineSymbols.otf /usr/share/fonts/
-fc-cache -vf
-mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 
-
+# composer-bin:
+php -r 'copy("https://composer.github.io/installer.sig", "/tmp/installer.sig");'
+php -r 'copy("https://getcomposer.org/installer", "composer-setup.php");'
+php -r "if (hash_file('sha384', 'composer-setup.php') === file_get_contents('/tmp/installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('/tmp/composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=bin --filename=composer
+php -r "unlink('composer-setup.php'); unlink('/tmp/installer.sig');"
 
 ############
 # TRAINING #
 ############
+
 # @see https://www.vimgolf.com/
 apt install -y rubygems && gem install vimgolf
