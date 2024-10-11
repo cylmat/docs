d9bceb21246b9aa8ffb9ac496e74fe3ef2d6237d
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-10-11 21:52:10 +0200

    Update create-symfony-project.sh

--- install/script/create-symfony-project.sh
+++ install/script/create-symfony-project.sh
@@ -3,12 +3,21 @@
 docker run -it -d -v .:/usr/src/myapp -w /usr/src/myapp --name phpapache --rm php:8.3-apache
 # apt
 docker exec sh -c "apt update && apt install -y git"
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
 # composer
 docker exec sh -c "curl -sS https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/composer-install.sh | bash"
 docker exec sh -c "mv ./composer.phar /usr/local/bin/composer"
 docker exec sh -c "rm composer-temp.phar && rm composer-setup.php"
+
 # symfony
 curl -sS https://get.symfony.com/cli/installer | bash
 mv /root/.symfony5/bin/symfony /usr/local/bin/symfony
-#
+symfony new project
+mv project/* .
+mv project/.* .
+rm -r project
+
+# run
 docker exec -it phpapache bash
