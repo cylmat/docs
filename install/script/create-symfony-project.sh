d32a15485f8beaead97d75328d865ad1f884a1ac
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-10-11 23:29:57 +0200

    Update create-symfony-project.sh

--- install/script/create-symfony-project.sh
+++ install/script/create-symfony-project.sh
@@ -32,7 +32,8 @@ docker exec sh -c "
 
 docker exec sh -c "
   cat composer.json | jq '.extra."public-dir"="html"' > /tmp/composer.json && \
-  rm composer.json && mv /tmp/composer.json .
+  rm composer.json && mv /tmp/composer.json . && \
+  composer req --dev maker
 "
 
 # run
