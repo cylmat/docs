43d40757610254a285a3834b60583d02e1b5ca1e
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-10-11 22:20:24 +0200

    Update create-symfony-project.sh

--- install/script/create-symfony-project.sh
+++ install/script/create-symfony-project.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-docker run -it -d -v .:/usr/src/myapp -w /usr/src/myapp --name phpapache --rm php:8.3-apache
+# docker container stop phpapache && docker container remove phpapache
+docker run -it -d -v .:/var/www/html -p 8123:80 --name phpapache php:8.3-apache
+
 # apt
 docker exec sh -c "apt update && apt install -y git"
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+docker exec sh -c "git config --global user.email 'you@example.com'"
+docker exec sh -c "git config --global user.name 'Your Name'"
 
 # composer
 docker exec sh -c "curl -sS https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/composer-install.sh | bash"
@@ -12,12 +14,15 @@ docker exec sh -c "mv ./composer.phar /usr/local/bin/composer"
 docker exec sh -c "rm composer-temp.phar && rm composer-setup.php"
 
 # symfony
-curl -sS https://get.symfony.com/cli/installer | bash
-mv /root/.symfony5/bin/symfony /usr/local/bin/symfony
-symfony new project
-mv project/* .
-mv project/.* .
-rm -r project
+docker exec sh -c "curl -sS https://get.symfony.com/cli/installer | bash"
+docker exec sh -c "mv /root/.symfony5/bin/symfony /usr/local/bin/symfony"
+docker exec sh -c "symfony new project"
+docker exec sh -c "mv project/* . && mv project/.* . && rm -r project"
+docker exec sh -c "chmod a+w -R ."
 
 # run
 docker exec -it phpapache bash
+
+return 0
+
+# npm if needed
