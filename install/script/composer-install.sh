036b4a698f50b0b7a690dd189849038e2332f1cb
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-10-11 21:46:32 +0200

    Update and rename composer.sh to composer-install.sh

--- install/script/composer-install.sh
+++ install/script/composer-install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Composer install
+echo "https://getcomposer.org/download/"
+
 EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
