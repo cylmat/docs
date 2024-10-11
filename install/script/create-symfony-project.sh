f447b9cae5ad1a03b3fea0e19c7e70a44454e075
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-10-11 22:41:00 +0200

    Update create-symfony-project.sh

--- install/script/create-symfony-project.sh
+++ install/script/create-symfony-project.sh
@@ -4,21 +4,32 @@
 docker run -it -d -v .:/var/www/html -p 8123:80 --name phpapache php:8.3-apache
 
 # apt
-docker exec sh -c "apt update && apt install -y git"
-docker exec sh -c "git config --global user.email 'you@example.com'"
-docker exec sh -c "git config --global user.name 'Your Name'"
+docker exec sh -c "
+  apt update && apt install -y git zip &&
+  git config --global user.email 'you@example.com' && \
+  git config --global user.name 'Your Name'
+"
 
 # composer
-docker exec sh -c "curl -sS https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/composer-install.sh | bash"
-docker exec sh -c "mv ./composer.phar /usr/local/bin/composer"
-docker exec sh -c "rm composer-temp.phar && rm composer-setup.php"
+docker exec sh -c "
+  curl -sS https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/composer-install.sh | bash && \
+  mv ./composer.phar /usr/local/bin/composer
+"
 
 # symfony
-docker exec sh -c "curl -sS https://get.symfony.com/cli/installer | bash"
-docker exec sh -c "mv /root/.symfony5/bin/symfony /usr/local/bin/symfony"
-docker exec sh -c "symfony new project"
-docker exec sh -c "mv project/* . && mv project/.* . && rm -r project"
-docker exec sh -c "chmod a+w -R ."
+docker exec sh -c "
+  curl -sS https://get.symfony.com/cli/installer | bash && \
+  mv /root/.symfony5/bin/symfony /usr/local/bin/symfony
+"
+
+# create
+docker exec sh -c "
+  symfony new project && \
+  mv project/* . && mv project/.* . && rm -r project && \
+  chmod a+w -R . && \
+  composer require symfony/apache-pack --no-interaction && \
+  curl https://github.com/symfony/recipes-contrib/blob/main/symfony/apache-pack/1.0/public/.htaccess -o public/.htaccess
+"
 
 # run
 docker exec -it phpapache bash
