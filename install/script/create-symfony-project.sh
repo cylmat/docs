d95c1a3d1cd3e4a846f8ddacc159b5c8cee9b6e9
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-10-12 21:50:36 +0200

    Update create-symfony-project.sh

--- install/script/create-symfony-project.sh
+++ install/script/create-symfony-project.sh
@@ -78,6 +78,7 @@ EOF
 
 docker build -t php-apache-img  .
 docker run -it -d -v .:/var/www -p 8123:80 --name phpapache php-apache-img
+chmod a+x -R ./scripts 
 echo 'Run "docker exec -it phpapache bash" to get into container.'
 echo 'Run scripts/create.sh to create a new Symfony project.'
 echo 'Run scripts/install_tests.sh to install Quality tests.'
