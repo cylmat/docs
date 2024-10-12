e27c0c706da95a686fb507c586b10360e9c8efd0
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-10-12 09:10:59 +0200

    Update create-symfony-project.sh

--- install/script/create-symfony-project.sh
+++ install/script/create-symfony-project.sh
@@ -1,48 +1,51 @@
 #!/usr/bin/env bash
 
+# docker container stop phpapache && docker container remove phpapache
+
+##########
+# Script #
+##########
+tee scripts/install.sh <<EOF
+#!/usr/bin/env bash
+
+# apt
+apt update && apt install -y git jq vim zip
+git config --global user.email 'you@example.com'
+git config --global user.name 'Your Name'
+
+# composer
+curl -sS https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/composer-install.sh | bash
+mv ./composer.phar /usr/local/bin/composer
+
+# symfony
+curl -sS https://get.symfony.com/cli/installer | bash
+mv /root/.symfony5/bin/symfony /usr/local/bin/symfony
+
+# create
+symfony new project
+mv project/* . && mv project/.* . && rm -r project
+mv project html
+chmod a+w -R .
+curl https://raw.githubusercontent.com/cylmat/symplay/refs/heads/main/public/.htaccess -o html/.htaccess
+
+# make
+cat composer.json | jq '.extra."public-dir"="html"' > /tmp/composer.json
+rm composer.json && mv /tmp/composer.json .
+composer req --dev maker
+EOF
+
+#######
+# Run #
+#######
 tee Dockerfile <<EOF
 FROM php:8.3-apache
 RUN cp /etc/apache2/mods-available/rewrite.load /etc/apache2/mods-enabled/
+CP ./scripts/install.sh /usr/local/bin/
+RUN source /etc/apache2/envvars
 EOF
-# docker build -t php-8.3-apache-img  .
-# docker exec source /etc/apache2/envvars
-# docker container stop phpapache && docker container remove phpapache
 
-docker run -dit -v .:/var/www/html -p 8123:80 --name phpapache php-8.3-apache-img
-
-# apt
-docker exec sh -c "
-  apt update && apt install -y git jq vim zip && \
-  git config --global user.email 'you@example.com' && \
-  git config --global user.name 'Your Name'
-"
-
-# composer
-docker exec sh -c "
-  curl -sS https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/composer-install.sh | bash && \
-  mv ./composer.phar /usr/local/bin/composer
-"
-
-# symfony
-docker exec sh -c "
-  curl -sS https://get.symfony.com/cli/installer | bash && \
-  mv /root/.symfony5/bin/symfony /usr/local/bin/symfony
-"
-
-# create
-docker exec sh -c "
-  symfony new project && \
-  mv project/* . && mv project/.* . && rm -r project && \
-  mv project html
-  chmod a+w -R . && \
-  curl https://raw.githubusercontent.com/cylmat/symplay/refs/heads/main/public/.htaccess -o html/.htaccess
-"
-
-docker exec sh -c "
-  cat composer.json | jq '.extra."public-dir"="html"' > /tmp/composer.json && \
-  rm composer.json && mv /tmp/composer.json . && \
-  composer req --dev maker
-"
+docker build -t php-8.3-apache-img  .
+docker run -it -d -v .:/var/www/html -p 8123:80 --name phpapache php-8.3-apache-img
 
 # run
 docker exec -it phpapache bash
