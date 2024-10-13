e6cf948a19040a3c5377a5a60b2374b8bda5a084
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-10-13 16:46:54 +0200

    Update qa_install.sh

--- install/script/create/scripts/qa_install.sh
+++ install/script/create/scripts/qa_install.sh
@@ -2,3 +2,9 @@
 
 mkdir -p tools
 composer req --dev --working-dir=tools friendsofphp/php-cs-fixer phpmd/phpmd phpunit/phpunit phpstan/phpstan
+
+echo "
+Suggested:
+https://github.com/jakzal/toolbox
+https://github.com/EdgedesignCZ/phpqa
+"
