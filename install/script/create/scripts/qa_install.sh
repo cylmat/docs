336fbcf3a8ba568af7719b13bb4d38fd233c0044
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-10-16 20:31:19 +0200

    Delete install/script/create/scripts/qa_install.sh

--- install/script/create/scripts/qa_install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p tools
-composer req --dev --working-dir=tools friendsofphp/php-cs-fixer phpmd/phpmd phpunit/phpunit phpstan/phpstan
-
-echo "See: [https://phpqa.io] and [https://github.com/easy-coding-standard]
-Suggested: [https://github.com/jakzal/toolbox] and [https://github.com/EdgedesignCZ/phpqa]"
