5cc2e44521f850432d1d8a4e0a78f664268b6b47
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-10-16 22:00:21 +0200

    Update create-symfony-project.sh

--- install/script/create-symfony-project.sh
+++ install/script/create-symfony-project.sh
@@ -6,23 +6,11 @@
 # @todo change user root
 # @todo add openssl
 
-##########
-# Script #
-##########
-
-mkdir -p scripts 
-
-curl https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/create/scripts/create_prj.sh -o scripts/create_prj.sh 
-curl https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/create/scripts/db_install.sh -o scripts/db_install.sh
-curl https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/create/scripts/qa_install.sh -o scripts/qa_install.sh 
-
-curl -O https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/create/Dockerfile
-curl -O https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/create/Makefile
-curl -O https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/create/Readme.md
+https://github.com/cylmat/clean-symfony .
+rm -r .git
 
 ### Usage doc ###
 
-chmod a+x -R ./scripts 
 cat Readme.md
 
 # npm if needed
