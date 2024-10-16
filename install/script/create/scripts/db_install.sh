ac3ad6678595b0fbafdac42c2f6cbbf817983725
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-10-16 20:30:42 +0200

    Delete install/script/create/scripts/db_install.sh

--- install/script/create/scripts/db_install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# doctrine
-composer req doctrine --no-interaction
-sed -ie 's/DATABASE_URL="postgresql/# DATABASE_URL="postgresql/' .env
-sed -ie 's/# DATABASE_URL="sqlite/DATABASE_URL="sqlite/' .env
-# form
-composer require form validator twig-bundle security-csrf
-echo 'Use bin/console make:entity or bin/console make:crud'
