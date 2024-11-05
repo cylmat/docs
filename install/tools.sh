36a6ed2d3c2907bd4f4b960af8281656b4b99339
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2024-11-05 19:27:04 +0100

    Update tools.sh

--- install/tools.sh
+++ install/tools.sh
@@ -287,7 +287,8 @@ php -r "unlink('composer-setup.php'); unlink('/tmp/installer.sig');"
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
-# kdash
+### kdash
+# brew install kdash-rs/kdash/kdash
 sudo apt install -y kdash
 
 ############
