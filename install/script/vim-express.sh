00bf9c0c0c073eb6e688ab999c66f44dce0089d9
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2025-03-12 22:10:03 +0100

    Update vim-express.sh

--- install/script/vim-express.sh
+++ install/script/vim-express.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-# usage curl 
+# usage:
+#
+# apt install -y curl git
+# curl https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/vim-express.sh | bash
 
 PLUGIN_DIR=~/.vim/pack/plugins/start
 
 # create 
+echo "Run"
 echo "create ~/.vim/pack/plugins/start"
+echo ""
+
+# mkdir -p ~/.vim/pack/plugins/start
 mkdir -p $PLUGIN_DIR
 
 # plugins
