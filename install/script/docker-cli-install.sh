a2a85aedd72b3ed4760aa8d607a7a1e2eaa00383
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2025-04-26 22:05:15 +0200

    Update docker-cli-install.sh

--- install/script/docker-cli-install.sh
+++ install/script/docker-cli-install.sh
@@ -1,15 +1,54 @@
 #!/bin/bash
 
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+# ###
+# Install from:
+# https://www.paulsblog.dev/how-to-install-docker-without-docker-desktop-on-windows/
+# ###
 
-# Add the repository to Apt sources:
+# 1. Required dependencies 
+sudo apt-get update 
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release 
+
+# 2. GPG key 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg 
+
+# 3. Use stable repository for Docker 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+  
+#echo \ 
+#  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \ 
+#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+
+# 4. Install Docker 
+sudo apt-get update 
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io 
+
+# 5. Add user to docker group 
+sudo groupadd docker 
+sudo usermod -aG docker $USER 
+
+
+
+
+
+
+# ### (or see https://docs.docker.com/engine/install/ubuntu)
+
+
+#sudo apt-get update
+#sudo apt-get install -y ca-certificates curl
+#sudo install -m 0755 -d /etc/apt/keyrings
+#sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+#sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+#echo \
+#  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+#  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+#  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo apt-get update
+
+#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#  sudo docker run hello-world
