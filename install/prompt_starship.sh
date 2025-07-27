27fd5c2712cc29142580590ebaa384c657d879c3
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2025-07-27 19:29:47 +0200

    Create prompt_starship.sh

---
+++ install/prompt_starship.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+############
+# Starship #
+############
+
+# Powerline FONT
+sudo apt install -y fonts-powerline
+sudo wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+sudo mv PowerlineSymbols.otf /usr/share/fonts/
+sudo fc-cache -vf
+sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
+
+### Starship @https://starship.rs
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+mkdir -p ~/.config && touch ~/.config/starship.toml
