fbd58a5980bb33ce50dd00693365f947fbac174c
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2025-07-27 21:32:55 +0200

    Update prompt_powerlevel.sh

--- install/prompt_powerlevel.sh
+++ install/prompt_powerlevel.sh
@@ -5,3 +5,12 @@
 ############
 
 # https://medium.com/@satriajanaka09/setup-zsh-oh-my-zsh-powerlevel10k-on-ubuntu-20-04-c4a4052508fd
+
+sudo apt-get install fonts-powerline
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# in ~/.zshrc
+# ZSH_THEME="powerlevel10k/powerlevel10k"
+
+p10k configure
