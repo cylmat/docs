78e84c60ce640aa833f84b60bd18edb4335de60a
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2025-08-02 21:52:17 +0200

    Update homebrew-install.sh

--- install/script/homebrew-install.sh
+++ install/script/homebrew-install.sh
@@ -12,14 +12,14 @@ fi
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 GREP=$(grep 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' $RC)
-if [[ ! -z GREP ]]; then
+if [[ -z GREP ]]; then
     echo "Add eval Homebrew in $RC"
     echo >> $RC
     echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $RC
 fi
 
 GREP=$(grep 'PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' $RC)
-if [[ ! -z GREP ]]; then
+if [[ -z GREP ]]; then
    echo "Add PATH Homebrew in $RC"
    echo 'PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >> $RC
 fi
