cb7fa0243de98158d3eabf746ec1c1f05b4845b9
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2025-08-03 22:54:56 +0200

    Update prompt_starship.sh

--- install/prompt_starship.sh
+++ install/prompt_starship.sh
@@ -7,12 +7,6 @@
 # FONT
 # curl https://raw.githubusercontent.com/cylmat/docs/refs/heads/main/install/script/nerdfonts-install.sh | bash
 
-# Powerline FONT
-sudo apt install -y fonts-powerline
-sudo wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
-sudo mv PowerlineSymbols.otf /usr/share/fonts/
-sudo fc-cache -vf
-sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
 
 RC="$HOME/.bashrc"
 if [[ "$SHELL" == '/bin/zsh' ]]; then
