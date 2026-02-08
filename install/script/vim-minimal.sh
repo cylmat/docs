a3ac25815e94a8fc234017736f4a8deee6eef60e
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2026-02-08 19:16:55 +0100

    Update vim-minimal.sh

--- install/script/vim-minimal.sh
+++ install/script/vim-minimal.sh
@@ -27,13 +27,13 @@ mkdir -p $PLUGIN_DIR
 echo "Installing plugins..."
 
 echo ":h NERDTree"
-test -d $PLUGIN_DIR/nerdtree       || git clone --depth 1https://github.com/scrooloose/nerdtree $PLUGIN_DIR/nerdtree
+test -d $PLUGIN_DIR/nerdtree       || git clone --depth 1 https://github.com/scrooloose/nerdtree $PLUGIN_DIR/nerdtree
 echo ":h easymotion (default '\')"
-test -d $PLUGIN_DIR/vim-easymotion || git clone --depth 1https://github.com/easymotion/vim-easymotion.git $PLUGIN_DIR/vim-easymotion
+test -d $PLUGIN_DIR/vim-easymotion || git clone --depth 1 https://github.com/easymotion/vim-easymotion.git $PLUGIN_DIR/vim-easymotion
 echo ":h sneak"
-test -d $PLUGIN_DIR/vim-sneak      || git clone --depth 1https://github.com/justinmk/vim-sneak $PLUGIN_DIR/vim-sneak
+test -d $PLUGIN_DIR/vim-sneak      || git clone --depth 1 https://github.com/justinmk/vim-sneak $PLUGIN_DIR/vim-sneak
 echo ":h surround"
-test -d $PLUGIN_DIR/vim-surround   || git clone --depth 1https://github.com/tpope/vim-surround $PLUGIN_DIR/vim-surround
+test -d $PLUGIN_DIR/vim-surround   || git clone --depth 1 https://github.com/tpope/vim-surround $PLUGIN_DIR/vim-surround
 
 
 echo "Listing..."
